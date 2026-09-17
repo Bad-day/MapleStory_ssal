032136e9fd18c7c429f75682367cafd309bef30b
Jeong Myeong Sup <75732202+Bad-day@users.noreply.github.com>
2026-09-18 00:38:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,14 @@
 /* 메소 알러지 쌀숭이 서비스 워커
    앱 껍데기를 캐시해 오프라인에서도 켜지게 한다.
    사진 판독과 넥슨 조회만 네트워크가 필요하고 나머지 기능은 전부 동작한다. */
-const CACHE = "ssalsungi-v1";
+const CACHE = "ssalsungi-v2";
 const SHELL = [
   "./", "./index.html", "./app.js", "./app.css",
   "./manifest.webmanifest", "./icon-192.png", "./icon-512.png",
 ];
+
+/* 인식 엔진 파일은 15MB 가까이 되므로 설치 때 미리 받지 않는다.
+   처음 읽기를 누를 때 내려받고, 그 뒤로는 캐시에서 꺼내 쓴다. */
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting()));
